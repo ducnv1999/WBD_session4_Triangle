503dc66c9af8f2a68f9d4ca4540ca92cd7dd2198
admin <ducgau99@gmail.com>
2019-04-03 16:49:48 +0800

    finish

---
+++ src/main/java/com/codegym/Triangletest.java
@@ -0,0 +1,14 @@
+package com.codegym;
+
+public class Triangletest {
+    public static void main(String[] args) {
+
+            Triangle triangle = new Triangle();
+            System.out.println(triangle);
+
+            triangle = new Triangle(3, 9, 5);
+            System.out.println(triangle);
+        }
+
+    }
+
